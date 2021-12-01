8887907f6038d28b230f184054d45f43073045ac
Ivan <ivanfurlong@gmail.com>
2021-12-01 00:20:26 -0800

    Fixed Tower Defense Inventory

--- Assets/Scripts/TowerDefense/TowerShop.cs
+++ Assets/Scripts/TowerDefense/TowerShop.cs
@@ -7,23 +7,23 @@ using UnityEngine;
 public class TowerShop : MonoBehaviour
 {
     private List<TowerShopItem> _shopItems;
-    public InventoryTracker playerInventory;
+    public InventoryManager playerInventory;
     public GameObject cancelItemDragging;
     void Start()
     {
         _shopItems = GetComponentsInChildren<TowerShopItem>().ToList();
         if (playerInventory == null)
         {
-            playerInventory = GameObject.Find("Player Inventory").GetComponent<InventoryTracker>();
+            playerInventory = GameObject.Find("Player Inventory").GetComponent<InventoryManager>();
         }
     }
 
     public bool PurchaseTower(InventoryTracker itemCost)
     {
-        int playerGreenCount = Int32.Parse(playerInventory.GreenCount.text);
-        int playerBlueCount = Int32.Parse(playerInventory.BlueCount.text);
-        int playerPurpleCount = Int32.Parse(playerInventory.PurpleCount.text);
-        int playerRedCount = Int32.Parse(playerInventory.RedCount.text);
+        int playerGreenCount = playerInventory.greenGem;
+        int playerBlueCount = playerInventory.blueGem;
+        int playerPurpleCount = playerInventory.purpleGem;
+        int playerRedCount = playerInventory.redGem;
 
         int costGreenCount = Int32.Parse(itemCost.GreenCount.text);
         int costBlueCount = Int32.Parse(itemCost.BlueCount.text);
@@ -34,10 +34,11 @@ public class TowerShop : MonoBehaviour
                            (playerPurpleCount >= costPurpleCount) && (playerRedCount >= costRedCount);
         if (canPurchase)
         {
-            playerInventory.GreenCount.text = (playerGreenCount - costGreenCount).ToString();
-            playerInventory.BlueCount.text = (playerBlueCount - costBlueCount).ToString();
-            playerInventory.PurpleCount.text = (playerPurpleCount - costPurpleCount).ToString();
-            playerInventory.RedCount.text = (playerRedCount - costRedCount).ToString();
+            // playerInventory.GreenCount.text = (playerGreenCount - costGreenCount).ToString();
+            // playerInventory.BlueCount.text = (playerBlueCount - costBlueCount).ToString();
+            // playerInventory.PurpleCount.text = (playerPurpleCount - costPurpleCount).ToString();
+            // playerInventory.RedCount.text = (playerRedCount - costRedCount).ToString();
+            playerInventory.GemTransaction(new int[] {costGreenCount, costBlueCount, costPurpleCount, costRedCount});
             return true;
         }
         return false;
