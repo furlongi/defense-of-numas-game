b53c03bcc4c93d680fa3608eeafe2c07ef5c04e9
Jason <lowe.d.jason@gmail.com>
2021-12-03 15:36:47 -0800

    Implemented Wave 1 rounds and updated tower balance

--- Assets/Scripts/TowerDefense/TowerCostData.cs
+++ Assets/Scripts/TowerDefense/TowerCostData.cs
@@ -7,24 +7,24 @@
         
         public static int[][] LightTower =
         {
-            new int[] {0, 0, 0, 0},    // Initial purchase
-            new int[] {0, 0, 0, 0},   // Tier 1
+            new int[] {20, 3, 0, 0},    // Initial purchase
+            new int[] {12, 5, 2, 0},   // Tier 1
             new int[] {40, 25, 10, 5},  // Tier 2
             new int[] {50, 30, 15, 7},  // Tier 3
         };
 
         public static int[][] MediumTower =
         {
-            new int[] {0, 0, 0, 0},    // Initial purchase
-            new int[] {0, 0, 0, 0},    // Tier 1
+            new int[] {15, 2, 0, 0},    // Initial purchase
+            new int[] {12, 4, 1, 0},    // Tier 1
             new int[] {0, 0, 0, 0},    // Tier 2
             new int[] {0, 0, 0, 0},    // Tier 3
         };
         
         public static int[][] HeavyTower =
         {
-            new int[] {0, 0, 0, 0},    // Initial purchase
-            new int[] {30, 15, 5, 0},   // Tier 1
+            new int[] {15, 2, 0, 0},    // Initial purchase
+            new int[] {12, 4, 1, 0},   // Tier 1
             new int[] {40, 25, 10, 5},  // Tier 2
             new int[] {50, 30, 15, 7},  // Tier 3
         };
