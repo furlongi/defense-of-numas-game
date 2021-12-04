a687b3997a0d1fd64ab57aa75b25402ee4415187
Jason <lowe.d.jason@gmail.com>
2021-12-03 16:09:31 -0800

    Balanced and completed wave one

--- Assets/Scripts/TowerDefense/TowerTierDamageData.cs
+++ Assets/Scripts/TowerDefense/TowerTierDamageData.cs
@@ -6,7 +6,7 @@ public static class TowerTierDamageData
         
     public static float[] LightTower =
     {
-        12f,    // Initial purchase
+        25f,    // Initial purchase
         24f,    // Tier 1
         36f,    // Tier 2
         48f,    // Tier 3
@@ -14,7 +14,7 @@ public static class TowerTierDamageData
     
     public static float[] MediumTower =
     {
-        10f,    // Initial purchase
+        8.3f,    // Initial purchase
         20f,    // Tier 1
         30f,    // Tier 2
         40f,    // Tier 3
@@ -22,10 +22,10 @@ public static class TowerTierDamageData
 
     public static float[] HeavyTower =
     {
-        7f,    // Initial purchase
-        14f,    // Tier 1
-        21f,    // Tier 2
-        28f,    // Tier 3
+        2.5f,    // Initial purchase
+        6f,    // Tier 1
+        9f,    // Tier 2
+        12f,    // Tier 3
     };
 
 }
