ad3fd8faf9f052147504b7725964335824a451a2
Jason <lowe.d.jason@gmail.com>
2021-12-05 19:29:33 -0800

    Updated Tower cost and damage balance

--- Assets/Scripts/TowerDefense/TowerCostData.cs
+++ Assets/Scripts/TowerDefense/TowerCostData.cs
@@ -7,26 +7,26 @@
         
         public static int[][] LightTower =
         {
-            new int[] {20, 3, 0, 0},    // Initial purchase
-            new int[] {12, 5, 2, 0},   // Tier 1
-            new int[] {40, 25, 10, 5},  // Tier 2
-            new int[] {50, 30, 15, 7},  // Tier 3
+            new int[] {17, 3, 0, 0},    // Initial purchase
+            new int[] {15, 7, 3, 0},    // Tier 1
+            new int[] {10, 10, 9, 3},  // Tier 2
+            new int[] {10, 10, 10, 10},  // Tier 3
         };
 
         public static int[][] MediumTower =
         {
             new int[] {15, 2, 0, 0},    // Initial purchase
-            new int[] {12, 4, 1, 0},    // Tier 1
-            new int[] {0, 0, 0, 0},    // Tier 2
-            new int[] {0, 0, 0, 0},    // Tier 3
+            new int[] {11, 6, 3, 0},    // Tier 1
+            new int[] {8, 8, 7, 2},     // Tier 2
+            new int[] {9, 9, 9, 9},     // Tier 3
         };
         
         public static int[][] HeavyTower =
         {
-            new int[] {15, 2, 0, 0},    // Initial purchase
-            new int[] {12, 4, 1, 0},   // Tier 1
-            new int[] {40, 25, 10, 5},  // Tier 2
-            new int[] {50, 30, 15, 7},  // Tier 3
+            new int[] {14, 2, 0, 0},    // Initial purchase
+            new int[] {11, 6, 3, 0},    // Tier 1
+            new int[] {8, 8, 7, 2},  // Tier 2
+            new int[] {9, 9, 9, 9},  // Tier 3
         };
 
     }
--- Assets/Scripts/TowerDefense/TowerTierDamageData.cs
+++ Assets/Scripts/TowerDefense/TowerTierDamageData.cs
@@ -7,25 +7,25 @@ public static class TowerTierDamageData
     public static float[] LightTower =
     {
         25f,    // Initial purchase
-        24f,    // Tier 1
-        36f,    // Tier 2
-        48f,    // Tier 3
+        50f,    // Tier 1
+        80f,    // Tier 2
+        110f,    // Tier 3
     };
     
     public static float[] MediumTower =
     {
         8.3f,    // Initial purchase
-        20f,    // Tier 1
-        30f,    // Tier 2
-        40f,    // Tier 3
+        16.6f,    // Tier 1
+        26f,    // Tier 2
+        36.6f,    // Tier 3
     };
 
     public static float[] HeavyTower =
     {
         2.5f,    // Initial purchase
-        6f,    // Tier 1
-        9f,    // Tier 2
-        12f,    // Tier 3
+        5f,    // Tier 1
+        8f,    // Tier 2
+        11f,    // Tier 3
     };
 
 }
